3dd65514941aa6c2e1e407a8492d1047142bd521
Lucas Vieira <lucas.engen.cc@gmail.com>
2021-01-30 15:02:38 -0300

    feat: :sparkles: add method to calculate distance between two objects on game map
    Signed-off-by: Lucas Vieira <lucas.engen.cc@gmail.com>

--- controller/objects.go
+++ controller/objects.go
@@ -4,7 +4,11 @@ import (
 	"strings"
 
 	"github.com/lucas-engen/WarTelemetry/model/mapobjects"
+	"github.com/lucas-engen/WarTelemetry/utils"
 )
+
+// distanceFactor constains the multiplication factor to be used on distance calculation
+const distanceFactor float64 = 66.32072
 
 // IsAircraft function determines if an object is of type aircraft
 func IsAircraft(object *mapobjects.MapObjects) bool {
@@ -27,4 +31,25 @@ func IsCurrentPlayer(object *mapobjects.MapObjects) bool {
 	}
 
 	return false
+}
+
+// CalculateDistanceInKm gets the distance in kilometers between two objects
+func CalculateDistanceInKm(obj1, obj2 *mapobjects.MapObjects) float64 {
+
+	// Convert object positions into a 2D vector
+	obj1Vector := utils.Vector2D{
+		X: float64(obj1.X),
+		Y: float64(obj1.Y),
+	}
+
+	obj2Vector := utils.Vector2D{
+		X: float64(obj2.X),
+		Y: float64(obj2.Y),
+	}
+
+	// Calculates the distance
+	distance := utils.CalculateDistance(obj1Vector, obj2Vector)
+
+	// Convert distance to kilometers
+	return distance * distanceFactor
 }
--- utils/math.go
+++ utils/math.go
@@ -2,7 +2,21 @@ package utils
 
 import "math"
 
+// Vector2D is a bidimensional vector
+type Vector2D struct {
+	X float64
+	Y float64
+}
+
 // Hypotenuse function returns the hypotenuse of a triangle
 func Hypotenuse(a, b float64) float64 {
 	return math.Sqrt((a * a) + (b * b))
+}
+
+// CalculateDistance function calculates the distance between two points
+func CalculateDistance(p1, p2 Vector2D) float64 {
+	dx := math.Pow(p2.X-p1.X, 2)
+	dy := math.Pow(p2.Y-p1.Y, 2)
+
+	return math.Sqrt(dx + dy)
 }
