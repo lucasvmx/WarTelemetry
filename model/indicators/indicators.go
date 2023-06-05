b0f02311a75d8b70aee0c0be33d34e0d4e8ac96f
Lucas Vieira <lucas.engen.cc@gmail.com>
2023-06-05 02:10:32 -0300

    chore: :zap: atualiza model dos indicators
    Signed-off-by: Lucas Vieira <lucas.engen.cc@gmail.com>

--- model/indicators/indicators.go
+++ model/indicators/indicators.go
@@ -34,6 +34,7 @@ type Indicators struct {
 	ClockSec         float32 `json:"clock_sec"`
 	RpmMin           float32 `json:"rpm_min"`
 	RpmMin1          float32 `json:"rpm1_min"`
+	AngleOfAttacl    float32 `json:"aoa"`
 }
 
 var path string = "indicators"
