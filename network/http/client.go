2b7ac0ef432a6e5766a2ca4b9920a829bbc5a7d3
Lucas Vieira <lucas.engen.cc@gmail.com>
2023-07-29 16:37:50 -0300

    chore: :zap: improves error checking
    Signed-off-by: Lucas Vieira <lucas.engen.cc@gmail.com>

--- network/http/client.go
+++ network/http/client.go
@@ -1,16 +1,18 @@
 package network
 
 import (
+	"fmt"
 	"io"
-	"log"
 	"net/http"
+
+	"github.com/lucasvmx/WarTelemetry/logger"
 )
 
 // ret function sends a GET request to the specified URL
 func get(url string) (response *http.Response, err error) {
 	response, err = http.Get(url)
 	if err != nil {
-		log.Printf("[ERROR] Failed to send request because of: %v", err)
+		logger.LogError("Failed to send request because of: %v", err)
 		return nil, err
 	}
 
@@ -34,6 +36,10 @@ func GetDataFromURL(url string) (data []byte, err error) {
 	payload, err := get(url)
 	if err != nil {
 		return nil, err
+	}
+
+	if payload.StatusCode != http.StatusOK {
+		return nil, fmt.Errorf("received unexpected HTTP code: %v", payload.StatusCode)
 	}
 
 	// Reads the HTTP response body
